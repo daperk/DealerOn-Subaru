863aa92ed79a502dea4484d4aa044593b5b08bb1
Daniel Pena <163460072+daperk@users.noreply.github.com>
2024-09-18 00:51:48 +0000

    layout

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for anchor links
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 1000);
+      }
+  });
+});
